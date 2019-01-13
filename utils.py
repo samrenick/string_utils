0596b41500f87c0f5125bef721f43a7d6d162480
samrenick <rachelrenick@Rachels-MacBook-Pro.local>
2019-01-12 21:08:42 -0500

    add method to get lengths of each char in a string

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import sys
-
 def is_permutation(arg1, arg2):
     first_set = set(arg1)
     second_set = set(arg2)
@@ -22,6 +21,21 @@ def replace_spaces(input_string):
         updated_string += letter
     return updated_string
 
+def count_chars(input):
+    counts = dict()
+    for letter in input:
+        if(letter in counts):
+            counts[letter] += 1
+        elif(letter.isalpha()):
+            counts[letter] = 1
+    count_string = ""
+    for key, value in counts.items():
+        count_string += str(key)
+        count_string += str(value)
+    if(len(count_string) > len(input)):
+        return input
+    else:
+        return count_string
 try:
     arg1 = sys.argv[1]
 except:
@@ -32,8 +46,9 @@ try:
 except:
     print("Please use two arguments")
     arg2 = ""
-print(is_unique(arg1))
-print(is_permutation(arg1, arg2))
-print("input: " + arg1)
-print(arg2)
-print("output: " + replace_spaces(arg1))
+# print(is_unique(arg1))
+# print(is_permutation(arg1, arg2))
+# print("input: " + arg1)
+# print(arg2)
+# print("output: " + replace_spaces(arg1))
+print(count_chars(arg1))
